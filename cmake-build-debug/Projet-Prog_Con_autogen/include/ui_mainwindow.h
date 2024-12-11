a0fe45c68aaf4e64bb3a3e8bef59a63243451484
Dikoume Eboule Charles Darryl <charles.dikoume@2027.ucac-icam.com>
2024-12-11 21:28:28 +0100

    ...

--- cmake-build-debug/Projet-Prog_Con_autogen/include/ui_mainwindow.h
+++ cmake-build-debug/Projet-Prog_Con_autogen/include/ui_mainwindow.h
@@ -11,9 +11,13 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +26,14 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QTableWidget *tableWidget;
+    QListWidget *orderListWidget;
+    QTableWidget *stockTableWidget;
+    QWidget *controlBar;
+    QPushButton *startSimulationButton;
+    QPushButton *pauseButton;
+    QPushButton *speedUpButton;
+    QPushButton *resetButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -32,6 +44,48 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        tableWidget = new QTableWidget(centralwidget);
+        if (tableWidget->columnCount() < 3)
+            tableWidget->setColumnCount(3);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+        tableWidget->setGeometry(QRect(10, 10, 780, 400));
+        tableWidget->setColumnCount(3);
+        tableWidget->setRowCount(0);
+        orderListWidget = new QListWidget(centralwidget);
+        orderListWidget->setObjectName(QString::fromUtf8("orderListWidget"));
+        orderListWidget->setGeometry(QRect(10, 420, 780, 150));
+        stockTableWidget = new QTableWidget(centralwidget);
+        if (stockTableWidget->columnCount() < 2)
+            stockTableWidget->setColumnCount(2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        stockTableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem3);
+        QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
+        stockTableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem4);
+        stockTableWidget->setObjectName(QString::fromUtf8("stockTableWidget"));
+        stockTableWidget->setGeometry(QRect(10, 580, 780, 150));
+        stockTableWidget->setColumnCount(2);
+        stockTableWidget->setRowCount(0);
+        controlBar = new QWidget(centralwidget);
+        controlBar->setObjectName(QString::fromUtf8("controlBar"));
+        controlBar->setGeometry(QRect(10, 740, 780, 50));
+        startSimulationButton = new QPushButton(controlBar);
+        startSimulationButton->setObjectName(QString::fromUtf8("startSimulationButton"));
+        startSimulationButton->setGeometry(QRect(10, 10, 100, 30));
+        pauseButton = new QPushButton(controlBar);
+        pauseButton->setObjectName(QString::fromUtf8("pauseButton"));
+        pauseButton->setGeometry(QRect(120, 10, 100, 30));
+        speedUpButton = new QPushButton(controlBar);
+        speedUpButton->setObjectName(QString::fromUtf8("speedUpButton"));
+        speedUpButton->setGeometry(QRect(230, 10, 100, 30));
+        resetButton = new QPushButton(controlBar);
+        resetButton->setObjectName(QString::fromUtf8("resetButton"));
+        resetButton->setGeometry(QRect(340, 10, 100, 30));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -49,6 +103,20 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Restaurant", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("MainWindow", "Table", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("MainWindow", "Capacity", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QCoreApplication::translate("MainWindow", "Status", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = stockTableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem3->setText(QCoreApplication::translate("MainWindow", "Ingredient", nullptr));
+        QTableWidgetItem *___qtablewidgetitem4 = stockTableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem4->setText(QCoreApplication::translate("MainWindow", "Quantity", nullptr));
+        startSimulationButton->setText(QCoreApplication::translate("MainWindow", "Start Simulation", nullptr));
+        pauseButton->setText(QCoreApplication::translate("MainWindow", "Pause", nullptr));
+        speedUpButton->setText(QCoreApplication::translate("MainWindow", "Speed Up", nullptr));
+        resetButton->setText(QCoreApplication::translate("MainWindow", "Reset", nullptr));
     } // retranslateUi
 
 };
